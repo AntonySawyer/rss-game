d6341003e968deb4b3420a40f5d48c905583e0c4
Antony <A.Krutenok@gmail.com>
2019-01-09 00:02:24 +0300

    try fix gitpages

--- dist/build.js
+++ dist/build.js
@@ -1228,7 +1228,7 @@ __webpack_require__.r(__webpack_exports__);
 <header class="page-header">
 	<nav>
 		<ul>
-			<li><a href="../../screens/landingPage/index.html">Landing</a></li>
+			<li><a href="../../../index.html">Landing</a></li>
 			<li><a href="#modal">Choose spell</a></li>
 			<li><a href="#">Scoreboard</a></li>
 			<li><span id="currentScore"></span></li>
